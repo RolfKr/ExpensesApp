802752e31857f719e30b691765c06729afde8459
Rolf Kristian <rolf0609@gmail.com>
2020-04-16 18:26:47 +0200

    Initial Commit

---
+++ ExpensesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExpensesApp
+//
+//  Created by Rolf Kristian Andreassen on 16/04/2020.
+//  Copyright © 2020 Rolf Kristian Andreassen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
