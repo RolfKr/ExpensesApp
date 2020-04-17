9f34c93f6e3a111f090f07f0dd1f2a28086c88d7
Rolf K Andreassen <rolf0609@gmail.com>
2020-04-17 13:37:28 +0200

    Created settings VC

---
+++ ExpensesApp/Controller/SettingsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsViewController.swift
+//  ExpensesApp
+//
+//  Created by Rolf Kristian Andreassen on 17/04/2020.
+//  Copyright © 2020 Rolf Kristian Andreassen. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Constants.shared.setBackgroundGradient(for: view)
+    }
+
+}
