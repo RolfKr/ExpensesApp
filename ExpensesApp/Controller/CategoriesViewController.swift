96374f46b15b28a01cd886731f84d0e9b49faef5
Rolf K Andreassen <rolf0609@gmail.com>
2020-04-17 10:47:15 +0200

    Added categories and section headers in categoryVC.

---
+++ ExpensesApp/Controller/CategoriesViewController.swift
@@ -0,0 +1,71 @@
+//
+//  CategoriesViewController.swift
+//  ExpensesApp
+//
+//  Created by Rolf Kristian Andreassen on 16/04/2020.
+//  Copyright © 2020 Rolf Kristian Andreassen. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesViewController: UIViewController {
+
+    @IBOutlet weak var searchContainer: UIView!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    //https://www.mint.com/mint-categories
+    var categoryHeaders = ["Income", "Entertainment", "Education", "Shopping", "Personal Care", "Health & Fitness", "Kids", "Food & Dining", "Gifts & Donations", "Investments", "Bills & Utilities", "Transport", "Travel", "Fees & Charges", "Business Services"]
+    let categories = [
+        ["Paycheck", "Investment", "Returned Purchase", "Bonus", "Interest Income", "Reimbursement", "Rental Income"],
+        ["Arts", "Music", "Movies", "Newspaper & Magazines", "Games"],
+        ["Tuition", "Student Load", "Books & Supplies"],
+        ["Clothing", "Books", "Electronics & Software", "Hobbies", "Sporting Goods"],
+        ["Laundry", "Hair", "Spa"],
+        ["Dentist", "Doctor", "Eye care", "Pharmacy", "Health Insurance", "Gym", "Sports"],
+        ["Activities", "Allowance", "Baby Supplies", "Babysitter & Daycare", "Child Support", "Toys"],
+        ["Groceries", "Coffee shops", "Fast Food", "Restaurants", "Alcohol"],
+        ["Gift", "Charity"],
+        ["Deposit", "Withdrawal", "Dividends & Cap Gains", "Buy", "Sell"],
+        ["Television", "Home Phone", "Internet", "Mobile Phone", "Utilities"],
+        ["Gas & Fuel", "Parking", "Service & Auto Parts", "Auto Payment", "Auto Insurance"],
+        ["Air Travel", "Hotel", "Rental Car & Taxi", "Vacation"],
+        ["Service Fee", "Late Fee", "Finance Charge", "ATM Fee", "Bank Fee", "Commissions"],
+        ["Advertising", "Office Supplies", "Printing", "Shipping", "Legal"]
+        
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Constants.shared.setBackgroundGradient(for: view)
+        searchContainer.layer.cornerRadius = 20
+        
+    }
+    
+    @IBAction func addCategoryTapped(_ sender: UIButton) {
+        print("Add button tapped")
+    }
+    
+}
+
+extension CategoriesViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return categoryHeaders.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return categoryHeaders[section]
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories[section].count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CategoryCell
+        let category = categories[indexPath.section][indexPath.row]
+        cell.configureCell(image: UIImage(named: "restaurant")!, name: category)
+        return cell
+    }
+}
---
+++ ExpensesApp/View/CategoryCell.swift
@@ -0,0 +1,21 @@
+//
+//  CategoryCell.swift
+//  ExpensesApp
+//
+//  Created by Rolf Kristian Andreassen on 17/04/2020.
+//  Copyright © 2020 Rolf Kristian Andreassen. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    func configureCell(image: UIImage, name: String) {
+        categoryImage.image = image
+        categoryLabel.text = name
+    }
+
+}
